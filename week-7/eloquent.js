ffd22c7e500d414cc97cd461b38433d780efb6ff
Scott Southard <scottmsouthard@gmail.com>
2016-03-21 22:16:44 +0000

    Finish eloquent code

---
+++ week-7/eloquent.js
@@ -0,0 +1,33 @@
+var number = 20;
+number = number * 2;
+
+
+prompt("What's your favorite food?");
+// "Pie"
+console.log("Hey! That's my favorite food too!")
+// Hey! That's my favorite food too!
+
+
+
+  for (var height = "#"; height.length <= 7; height += "#")
+    console.log(height);
+
+
+min(a, b) {
+  if (a < b) {
+    return a
+  }
+  else if (b < a) {
+    return b
+  }
+  else {
+    return "Equal"
+  }
+}
+
+var me = {
+  name: "Scott",
+  foods: ["Pizza", "Burritos", "Burgers"],
+  quirk: "Too Handsome"
+}
+
